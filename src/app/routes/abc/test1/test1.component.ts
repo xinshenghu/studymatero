d7bca91107489b427fa93e15393cf5a245c44bdd
ranzhang2 <235389963@qq.com>
2023-05-11 18:16:14 +0800

    2

--- src/app/routes/abc/test1/test1.component.ts
+++ src/app/routes/abc/test1/test1.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test1.component.scss']
 })
 export class AbcTest1Component implements OnInit {
-
+  value = 1e21 + 1e2;
   constructor() { }
 
   ngOnInit() {
   }
+  a() {
 
+    const input = "1.23e10";
+    const scientificNotationRegex = /^[+-]?\d+(\.\d+)?([eE][+-]?\d+)$/;
+    const match = input.match(scientificNotationRegex);
+
+    if (match) {
+      // debugger
+      const exponentPart = match[2];
+      console.log(1 + exponentPart); // "+10"
+    } else {
+      console.log("Invalid scientific notation");
+    }
+  }
 }
